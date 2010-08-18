59d54ab2d9933a92af5ee1b81279ed673d21b030
Matthias Schneider <matthias.schneider@beached.org>
2010-08-18 15:13:56 +0200

    scheinbar was gscheites

--- db.py
+++ db.py
@@ -4,12 +4,13 @@ import sys
 import codecs
 import cPickle as pickle
 from suds.client import Client
+from operator import itemgetter
 
 '''
 TODO:
  - create ladder for current season
  - use eternal ladder?
- - 
+ - daten der aufsteiger aus 2. liga!!!
 '''
 
 class Meta:
@@ -20,33 +21,37 @@ class Meta:
     def __init__(self):
         self.matches_file = 'data/matches.pkl'
         self.timeline_file = 'data/score_timeline.pkl'
+        self.current_year = 2010
         self.years = [2004, 2005, 2006, 2007, 2008, 2009]
         self.maps = {2004: {},
                      2005: {},
                      2006: {},
                      2007: {},
                      2008: {},
-                     2009: {}}
+                     2009: {},
+                     2010: {}}
         self.score_timeline = {2004: {},
                                2005: {},
                                2006: {},
                                2007: {},
                                2008: {},
-                               2009: {}}
+                               2009: {},
+                               2010: {}}
 
 
 class Data:
     def __init__(self):
-        # unpickle data
         meta = Meta()
+        self.current_year = meta.current_year
         self.years = meta.years
-        self.maps = meta.maps
         self.matches_file = meta.matches_file
         self.timeline_file = meta.timeline_file
         self.cross_table = 0
         self.score_timeline = 0
 
         self.names = {65: '1. FC Köln',123: '1899 Hoffenheim',83: 'Arminia Bielefeld',6: 'Bayer Leverkusen',87: 'Bor. Mönchengladbach',7: 'Bor. Dortmund',91: 'Eintracht Frankfurt',93: 'Energie Cottbus',40: 'FC Bayern München',9: 'FC Schalke 04',100: 'Hamburger SV',55: 'Hannover 96',54: 'Hertha BSC',105: 'Karlsruher SC',16: 'VfB Stuttgart',129: 'VfL Bochum',131: 'VfL Wolfsburg',134: 'Werder Bremen',79: '1. FC Nürnberg',102: 'Hansa Rostock',107: 'MSV Duisburg',81: '1. FSV Mainz 05',23: 'Alemannia Aachen',76: '1. FC Kaiserslautern',112: 'SC Freiburg', 98: 'FC St. Pauli'}
+
+        self.eternal_ladder = {65: 159, 123: 175, 83: 181, 6: 260, 87: 165, 7: 244, 91: 185, 93: 167, 40: 355, 9: 306 ,100: 279, 55: 216, 54: 257, 105: 162, 16: 287, 129: 183, 131: 242, 134: 306, 79: 191, 102: 150, 107: 146, 81: 175, 23: 154, 76: 165, 112: 138, 98: 100}
 
         self.unpickle()
 
@@ -60,28 +65,131 @@ class Data:
         self.score_timeline = pickle.load(timeline_f)
         timeline_f.close()
 
+    def pickle(self):
+        matches_f = open(self.matches_file, 'wb')
+        pickle.dump(self.cross_table, matches_f)
+        matches_f.close()
+        score_timeline_f = open(self.timeline_file, 'wb')
+        pickle.dump(self.score_timeline, score_timeline_f)
+        score_timeline_f.close()
+
+    def insert_data(self, line, gameday):
+        match = [int(i) for i in line.split(' ')]
+
+        # timeline
+        self.score_timeline[self.current_year].setdefault(match[0], list())
+        self.score_timeline[self.current_year].setdefault(match[1], list())
+
+        # check if gameday already in db (after .setdefault!)
+        if len(self.score_timeline[self.current_year][match[0]]) >= gameday:
+            # error, gameday already inserted
+            return False
+
+        self.score_timeline[self.current_year][match[0]].append((match[2], match[3]))
+        self.score_timeline[self.current_year][match[1]].append((match[3], match[2]))
+
+        # cross table
+        self.cross_table[self.current_year].setdefault(match[0], dict({match[1]: list()}))
+        self.cross_table[self.current_year].setdefault(match[1], dict({match[0]: list()}))
+
+
+        if match[1] in self.cross_table[self.current_year][match[0]]:
+            self.cross_table[self.current_year][match[0]][match[1]].append([match[2], match[3]])
+        else:
+            self.cross_table[self.current_year][match[0]][match[1]] = [[match[2], match[3]]]
+
+        if match[0] in self.cross_table[self.current_year][match[1]]:
+            self.cross_table[self.current_year][match[1]][match[0]].append([match[3], match[2]])
+        else:
+            self.cross_table[self.current_year][match[1]][match[0]] = [[match[3], match[2]]]
+
+
+        return True
+
     # returns a list of all scores by given team
     def get_all_goals(self, team):
         all_goals = []
         for year, matches in self.cross_table.iteritems():
-            for enemy, scores in matches[team].iteritems():
-                all_goals.append(scores[0])
-                all_goals.append(scores[1])
-
+            try:
+                for enemy, scores in matches[team].iteritems():
+                    all_goals.append(scores[0])
+                    all_goals.append(scores[1])
+            except:
+                continue # if no goals are found
         return all_goals
 
+    def get_median(self, team, count=4):
+        goals = self.get_all_goals(team)
+        d = {}
+        for match in goals:
+            d.setdefault(tuple(match), 0)
+            d[tuple(match)] +=1
+
+        sorted_by_goals = sorted(d.items(), key=itemgetter(1), reverse=True)
+        return sorted_by_goals[:count]
+
     def get_homegoals_sum(self, team):
-        home_sum = sum([g[1] for g in self.get_all_goals(team)])
+        home_sum = sum([g[0] for g in self.get_all_goals(team)])
         return home_sum
+
+    def get_homegoals_avg(self, team):
+        home_sum = sum([g[0] for g in self.get_all_goals(team)])
+        if len(self.get_all_goals(team)):
+            return home_sum / float(len(self.get_all_goals(team)))
+        else:
+            return 0
         
-    def get_timeline(self, team):
-        pass
+    def get_awaygoals_sum(self, team):
+        away_sum = sum([g[1] for g in self.get_all_goals(team)])
+        return away_sum
+
+    def get_awaygoals_avg(self, team):
+        away_sum = sum([g[1] for g in self.get_all_goals(team)])
+        if len(self.get_all_goals(team)):
+            return away_sum / float(len(self.get_all_goals(team)))
+        else:
+            return 0
+
+    # returns a score timeline for each year
+    def get_timeline(self, team, year):
+        try:
+            tl = self.score_timeline[year][team]
+        except:
+            tl = []
+        return tl
+
+    def get_trend(self, team, timespan=3, year=2010):
+        games = self.get_timeline(team, year)[-timespan:]
+        if not games:
+            games = self.get_timeline(team, year-1)[-timespan:]
+        points = 0
+        for game in games:
+            if game[0]>game[1]:
+                points+=3
+            elif game[0]==game[1]:
+                points+=1
+
+        ratio =  points / float(timespan)
+
+        # TODO ratio threshold could be optimized
+        if ratio >= 1.5:
+            return 1
+        elif ratio < 1.5 and ratio >= 1.0:
+            return 0
+        else:
+            return -1
 
     # entweder reines ergebnis oder 1, 0, -1 (aus sicht des heimteams)
-    def compare_teams(self, team1, team2):
-        pass
+    def compare_teams(self, team1, team2, timespan=3):
+        r1 = self.get_trend(team1, timespan, self.current_year)
+        r2 = self.get_trend(team2, timespan, self.current_year)
+        if r1 > r2:
+            return 1
+        elif r1 == r2:
+            return 0
+        else:
+            return -1
 
-    
 
 
 class Parser:
@@ -136,7 +244,7 @@ class LigaDB:
         self.url = 'http://www.openligadb.de/Webservices/Sportsdata.asmx?WSDL'
         self.client = Client(self.url)
         self.league = 'bl1'
-        
+
         meta = Meta()
         self.years = meta.years
 
--- main.py
+++ main.py
@@ -5,22 +5,14 @@
 import sys
 import random
 import cPickle as pickle
-
+import db
 
 DEBUG = 1
 
 class Main:
     def __init__(self):
-        # read data
-        pass
-
-    def predict(self, teams):
-        pass
-
-   # add new scores and recalculate the stats
-    def add_and_do_magic(self, team, gameday):
-       pass
-
+        self.pred = Predictor()
+        self.data = db.Data()
 
     def main(self):
       # sys args
@@ -33,81 +25,151 @@ class Main:
          mode = sys.argv[1]
          gameday = int(sys.argv[2])
 
-      # read input
       input = sys.stdin.readlines()
 
       if mode == '--predict':
          # calculate every match
          for i in input:
-            match = i.replace('\n', '').split(' ')
-
-            # classic plus bonus method
-            self.predict(match)
+            match = i.strip('\n').split(' ')
+            self.generate_prediction(match)
 
       # verify last gameday and enter scores to database
       elif mode == '--verify':
          for i in input:
-            match = i.replace('\n', '').split(' ')
-
-            # add live scores to the database and recalculate
-            self.add_and_do_magic(match, gameday)
+             match = i.strip('\n')
+             self.new_scores(match, gameday)
 
       else:
-         print 'ERROR: arguments not recognized.\ndefault method called: solving the answer to the life, the universe and everything. come back in 7.5 million years. and thanks for all the fish!'
+         print >> sys.stderr, '\033[1;31mError:\033[1;m Arguments not recognized.'
 
 
-class Parser:
+    def generate_prediction(self, teams):
+        # call predictor here
+        pair = [int(t) for t in teams]
+        self.pred.predict2(pair)
+
+    # add new scores and recalculate the stats
+    def new_scores(self, line, gameday):
+        if self.data.insert_data(line, gameday):
+            self.data.pickle()
+        else:
+            print >> sys.stderr, '\033[1;31mError:\033[1;m This match is already inserted.'
+
+class Predictor:
     def __init__(self):
-        self.files = [2004, 2005, 2006, 2007, 2008, 2009]
-        self.maps = {2004: {},
-                2005: {},
-                2006: {},
-                2007: {},
-                2008: {},
-                2009: {}}
-        self.score_timeline = {2004: {},
-                2005: {},
-                2006: {},
-                2007: {},
-                2008: {},
-                2009: {}}
+        self.d = db.Data()
+        self.meta = db.Meta()
+        self.current_year = self.meta.current_year
+        
+        self.pres = [[0,0], [1,0], [0,1], [1,1], [2,0], [2,1], [1,2], [0,2], [2,2], [3,0], [0,3], [3,1], [1,3], [3,2], [2,3]]
+        self.pres_home = [[0,0], [1,0], [1,1], [2,0], [2,1], [2,2], [3,0], [3,1], [3,2]]
+        self.pres_away = [[0,0], [0,1], [1,1], [0,2], [1,2], [2,2], [0,3], [1,3], [2,3]]
+        self.pres_win = [[1,0], [2,0], [2,1], [3,0], [3,1], [3,2]]
 
-    def add_score(self, line, year):
-        self.maps[year].setdefault(line[0], dict({line[1]: list()}))
-        self.maps[year].setdefault(line[1], dict({line[0]: list()}))
+    # tendenz und allg. stärke
+    # this is officialy declared as BULLSHIT!
+    def predict(self, teams):
+        t1, t2 = teams[0], teams[1]
 
-        if line[1] in self.maps[year][line[0]]:
-            self.maps[year][line[0]][line[1]].append((line[2], line[3]))
+        # compare
+        cmpr = self.d.compare_teams(t1, t2)
+
+        # eternal ladder
+        t1_score, t2_score = self.d.eternal_ladder[t1], self.d.eternal_ladder[t2]
+        ratio = t1_score / float(t2_score)
+        if ratio > 1.0:
+            et_cmp = 1
         else:
-            self.maps[year][line[0]][line[1]] = list((line[2], line[3]))
-
-        if line[0] in self.maps[year][line[1]]:
-            self.maps[year][line[1]][line[0]].append((line[3], line[2]))
+            et_cmp = -1
+            
+        # offense/defense
+        t1_off, t1_def = self.d.get_homegoals_sum(t1), self.d.get_awaygoals_sum(t1)
+        if t1_def:
+            t1_gr = t1_off / float(t1_def)
         else:
-            self.maps[year][line[1]][line[0]] = list((line[3], line[2]))
+            t1_gr = 0
+        t2_off, t2_def = self.d.get_homegoals_sum(t2), self.d.get_awaygoals_sum(t2)
+        if t2_def:
+            t2_gr = t2_off / float(t2_def)
+        else:
+            t2_gr = 0
+        gr_diff = t1_gr - t2_gr
 
-    def add_score_timeline(self, line, year):
-        self.score_timeline[year].setdefault(line[0], list())
-        self.score_timeline[year].setdefault(line[1], list())
+        threshold = 0.4
+        if  gr_diff > threshold:
+            off_def_cmp = 1
+        elif gr_diff <= threshold and gr_diff > (-1*threshold):
+            off_def_cmp = 0
+        else:
+            off_def_cmp = -1
 
-        self.score_timeline[year][line[0]].append((line[2], line[3]))
-        self.score_timeline[year][line[1]].append((line[3], line[2]))
+        # avg goals
+        g1_avg = (round(self.d.get_homegoals_avg(t1), 3), round(self.d.get_awaygoals_avg(t1),3))
+        g2_avg = (round(self.d.get_homegoals_avg(t2), 3), round(self.d.get_awaygoals_avg(t2), 3))
 
-    def parse(self):        
-        for year in self.files:
-            fd = open('data/matches'+str(year)+'.dat')
-            fd.seek(0)
-            for line in fd.readlines():
-                line_split = line.split(' ')
-                self.add_score([int(i) for i in line_split], year)
-                self.add_score_timeline([int(i) for i in line_split], year)
+        print self.d.names[t1], self.d.names[t2]
+        print 'cmpr', cmpr, 'off_def_cmp', off_def_cmp, 'et_cmp', et_cmp, 'goals', g1_avg, ':', g2_avg
 
-        maps_f = open('data/matches.pkl', 'wb')
-        pickle.dump(self.maps, maps_f)
-        maps_f.close()
-        score_timeline_f = open('data/score_timeline.pkl', 'wb')
-        pickle.dump(self.score_timeline, score_timeline_f)
-        score_timeline_f.close()
-                
-                
-                
+        # falls cmpr 0, und noch ein weitres 0 -> unentschieden
+        # falls cmpr 1, und ein weiteres 1, -> sieg heim
+        # falls cmpr -1 und ein weitres -1 -> sieg gast
+
+
+    # median als basis
+    def predict2(self, teams):
+        t1, t2 = teams[0], teams[1]
+
+        # 1. basis
+        t1_median = self.d.get_median(t1)
+        t2_median = self.d.get_median(t2)
+        t1_mres = []
+        for res in t1_median:
+            t1_mres.append(res[0])
+        t2_mres = []
+        for res in t2_median:
+            t2_mres.append(res[0])
+
+        # 2. aktuelle leistung
+        t1_recent = self.d.get_timeline(t1, self.current_year)[-3:]
+        t2_recent = self.d.get_timeline(t2, self.current_year)[-3:]        
+        
+        # häufigstes gemeinsames ergebnis
+        match = 0
+        for res in t1_mres:
+            if res in t2_mres:
+                match = res
+                break
+            else:
+                continue
+        if not match:
+            match = t1_mres[0]
+        match = list(match) # zur liste casten
+
+        # trend
+        t1_trend = self.d.get_trend(t1)
+        t2_trend = self.d.get_trend(t2)
+        trend = t1_trend -t2_trend
+        if trend == 0:
+            pass # keine änderung
+        elif trend == 1:
+            match[0]+=1
+        elif trend == 2 :
+            match[0]+= 1
+            match[1]-= 1
+        elif trend == -1:
+            match[1]+= 1
+        elif trend == -2:
+            match[0]-= 1
+            match[1]+= 1
+
+        
+        # und ab damit
+        self.print_scores(t1, t2, match)    
+
+    def print_scores(self, t1, t2, score):
+        print t1, t2, score[0], score[1]
+            
+            
+if __name__ == '__main__':
+   m = Main()
+   m.main()
